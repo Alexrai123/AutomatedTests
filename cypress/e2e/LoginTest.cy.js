664433a47127ffa2fdedc670c05f36187fcf90c0
Alexrai123 <alexhudita@gmail.com>
2024-03-18 02:18:31 +0200

    Signup and login with new account

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -2,7 +2,7 @@ describe("Test suite 1", () => {
   beforeEach(() => {
     cy.visit("https://frontend-delta-one-79.vercel.app/");
   });
-
+  /*
   it("Test 1: Try to login.", function () {
     cy.get(".ChoiceLabel").click();
     cy.get("#email").type("alex@gmail.com");
@@ -16,5 +16,18 @@ describe("Test suite 1", () => {
     cy.get("#email").type("test1@gmail.com");
     cy.get("#password").type("123");
     cy.get(".button-SignUp").click();
+  });
+  */
+  it("Test 3: Try to signup then login with new account.", function () {
+    cy.get(".ChoiceLabel1").click();
+    cy.get("#name").type("test1");
+    cy.get("#email").type("test1@gmail.com");
+    cy.get("#password").type("123");
+    cy.get(".button-SignUp").click();
+    cy.visit("https://frontend-delta-one-79.vercel.app/");
+    cy.get(".ChoiceLabel").click();
+    cy.get("#email").type("test1@gmail.com");
+    cy.get("#password").type("123");
+    cy.get(".button-Login").click();
   });
 });
