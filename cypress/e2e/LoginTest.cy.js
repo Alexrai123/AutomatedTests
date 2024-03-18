c9dee048c773986c9b26f9a9444f92e27bcb2cdd
Alexrai123 <alexhudita@gmail.com>
2024-03-18 19:35:09 +0200

    3 Tests

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,30 +1,38 @@
+Cypress.Commands.add("login", (email, password) => {
+  cy.get(".ChoiceLabel").click();
+  cy.get("#email").type(email);
+  cy.get("#password").type(password);
+  cy.get(".button-Login").click();
+});
+
+Cypress.Commands.add("signup", (name, email, password) => {
+  cy.get(".ChoiceLabel1").click();
+  cy.get("#name").type(name);
+  cy.get("#email").type(email);
+  cy.get("#password").type(password);
+  cy.get(".button-SignUp").click();
+});
+
 describe("Test suite 1", () => {
   beforeEach(() => {
-    cy.visit("https://frontend-delta-one-79.vercel.app/");
+    cy.visit("http://localhost:3000");
   });
-  /*
+
   it("Test 1: Try to login.", function () {
-    cy.get(".ChoiceLabel").click();
-    cy.get("#email").type("alex@gmail.com");
-    cy.get("#password").type("123");
-    cy.get(".button-Login").click();
+    cy.login("admin@new.com", "admin");
   });
 
   it("Test 2: Try to signup.", function () {
-    cy.get(".ChoiceLabel1").click();
-    cy.get("#name").type("test1");
-    cy.get("#email").type("test1@gmail.com");
-    cy.get("#password").type("123");
-    cy.get(".button-SignUp").click();
+    cy.signup("test1", "test1@gmail.com", "123");
   });
-  */
+
   it("Test 3: Try to signup then login with new account.", function () {
     cy.get(".ChoiceLabel1").click();
     cy.get("#name").type("test1");
     cy.get("#email").type("test1@gmail.com");
     cy.get("#password").type("123");
     cy.get(".button-SignUp").click();
-    cy.visit("https://frontend-delta-one-79.vercel.app/");
+    cy.visit("http://localhost:3000");
     cy.get(".ChoiceLabel").click();
     cy.get("#email").type("test1@gmail.com");
     cy.get("#password").type("123");
